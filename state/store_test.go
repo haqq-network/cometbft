39c646d270b930b71f3a1d1d378eb300bb4d3ea3
Evgeniy Abramov <i@kio.ninja>
2024-07-29 10:57:00 +0400

    feat: genesis custom initial for fork logic

--- state/store_test.go
+++ state/store_test.go
@@ -56,7 +56,7 @@ func BenchmarkLoadValidators(b *testing.B) {
 	stateStore := sm.NewStore(stateDB, sm.StoreOptions{
 		DiscardABCIResponses: false,
 	})
-	state, err := stateStore.LoadFromDBOrGenesisFile(config.GenesisFile())
+	state, err := sm.MakeGenesisStateFromFile(config.GenesisFile())
 	if err != nil {
 		b.Fatal(err)
 	}
